70feb22c5962ba83a86a4946f544cec29a90f340
MichaelQiu <qiuxiaolei1992@163.com>
2016-01-13 22:41:17 -0800

    完成基本的功能，包括游戏次数显示，产生随机目标，计算得分

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,26 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        startNewRound()
+        updateLabel()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
     }
     
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World", message: "This is my first app", preferredStyle: UIAlertControllerStyle.Alert)
+//        var difference: Int
+//        if currentValue > targetValue {
+//            difference = currentValue - targetValue
+//        } else if currentValue < targetValue {
+//            difference = targetValue - currentValue
+//        } else {
+//            difference = 0
+//        }
         
-        let action = UIAlertAction(title: "Awesome", style: UIAlertActionStyle.Default, handler: nil)
+//        let difference = (currentValue > targetValue) ? currentValue - targetValue : targetValue - currentValue
         
+//        var difference = currentValue - targetValue
+//        if difference < 0 {
+//            difference = -difference
+//        }
+        
+        let difference = abs(currentValue - targetValue)
+        let points = 100 - difference
+        score += points
+//        let message = "The value of slider is: \(currentValue)"
+//                    + "\nThe target value is: \(targetValue)"
+//                    + "\nThe difference is: \(difference)"
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
         alert.addAction(action)
-        
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+        updateLabel()
     }
 
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+//        targetLabel.text = "\(targetValue)"
+    }
 }
 
